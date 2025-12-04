7025c29cc868183f1f2f1a3263b64909da8c832d
omar-khaled67 <ok672005@gmail.com>
2025-12-04 19:22:00 +0200

    Update API URL to use environment variable

--- Frontend/src/requestMethod.js
+++ Frontend/src/requestMethod.js
@@ -1,11 +1,11 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:8000/api/";
+const BASE_URL = `${process.env.REACT_APP_API_URL}/api/`;
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL
 })
 
 export const userRequest = axios.create({
-    
+    headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
     baseURL: BASE_URL
 })
