4817f6ed08487828e1ac2e9b23e9310d004cec7b
omar-khaled67 <ok672005@gmail.com>
2025-12-04 19:03:36 +0200

    Update login API URL to use environment variable

--- Frontend/src/redux/apiCalls.js
+++ Frontend/src/redux/apiCalls.js
@@ -11,7 +11,7 @@ import {
 export const login = async (dispatch, userCredentials, navigate) => {
   dispatch(loginStart());
   try {
-    const res = await publicRequest.post("http://localhost:8000/api/auth/login", userCredentials);
+    const res = await publicRequest.post(`${process.env.REACT_APP_API_URL}/api/auth/login`, userCredentials);
     dispatch(loginSuccess(res.data));
     
     // السطر السحري اللي هيحل كل المشاكل
