4485ee2ce5cce32d1172a76a44832caa2f696880
omar-khaled67 <ok672005@gmail.com>
2025-12-04 15:35:48 +0200

    Update Admin.jsx

--- Frontend/src/pages/Admin.jsx
+++ Frontend/src/pages/Admin.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import axios from "axios";
-import Menu from "../components/Menu"; // لو عندك
-
+import Menu from "../components/Menu"; 
 function Admin() {
   const { currentUser } = useSelector((state) => state.user);
 
