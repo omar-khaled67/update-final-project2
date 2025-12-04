0d7dc799ec52e911792e25ea8ac3abde1ec154ab
omar-khaled67 <ok672005@gmail.com>
2025-12-04 19:02:46 +0200

    Update Login.jsx

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
     dispatch(loginStart());
 
     try {
-      const res = await fetch("http://localhost:8000/api/auth/login", {
+      const res = await fetch(`${process.env.REACT_APP_API_URL}/api/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
