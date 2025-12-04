4bc304f3ab23a45596dd8dccf2768089af83c750
omar-khaled67 <ok672005@gmail.com>
2025-12-04 21:37:22 +0200

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -7,7 +7,7 @@ const authRoute = require("./routes/auth");
 
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "https://update-final-project2-z857.vercel.app", 
   credentials: true
 }));
 app.use(express.json());
